2a7817914b34c348180b4c4342d0ef25f12295e3
Carine-69 <umugabecarine@gmail.com>
2024-10-15 16:18:59 +0200

    task 5

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Inserting a single row into the customers table in alx_book_store
+
+INSERT INTO customers (customer_id, customer_name, email, address) 
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
